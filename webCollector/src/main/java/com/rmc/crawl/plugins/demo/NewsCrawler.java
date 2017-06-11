9b10563229c6aa1c3bb0ba1f257c4aed82f39b14
welcome6688 <welcome66@163.com>
2017-06-11 23:10:04 +0800

    Add files via upload
    Add paging function

--- webCollector/src/main/java/com/rmc/crawl/plugins/demo/NewsCrawler.java
+++ webCollector/src/main/java/com/rmc/crawl/plugins/demo/NewsCrawler.java
@@ -27,7 +27,7 @@ public class NewsCrawler extends BreadthCrawler {
 
         /*fetch url like http://news.hfut.edu.cn/show-xxxxxxhtml*/
 //        this.addRegex("http://news.hfut.edu.cn/show-.*html");
-        this.addRegex("http://www.booktxt.net/2_2219/21.*html");
+        this.addRegex("http://www.booktxt.net/2_2219/2185566.*html");
         	
         /*do not fetch jpg|png|gif*/
         this.addRegex("-.*\\.(jpg|png|gif).*");
@@ -40,17 +40,20 @@ public class NewsCrawler extends BreadthCrawler {
         String url = page.getUrl();
         /*if page is news page*/
 //        if (page.matchUrl("http://news.hfut.edu.cn/show-.*html")) {
-        if (page.matchUrl("http://www.booktxt.net/2_2219/21.*html")) {	
+        StringBuffer str = new StringBuffer();
+        if (page.matchUrl("http://www.booktxt.net/2_2219/2185566.*html")) {	
             /*we use jsoup to parse page*/
             Document doc = page.getDoc();
 
             /*extract title and content of news by css selector*/
             String title = page.select("div[class=bookname]>h1").first().text();
-            String content = page.select("div#content", 0).text();
+//            str.append(page.select("div#content", 0).text().substring(0, 100));
+            String content = getRecordForPage(page.select("div#content", 0).text());
+//            String content = page.select("div#content", 0).text();
 //            content = content.replace("没了", "okok");
             System.out.println("URL:\n" + url);
             System.out.println("title:\n" + title);
-            System.out.println("content:\n" + content);
+//            System.out.println("content:\n" + content);
 
             /*If you want to add urls to crawl,add them to nextLink*/
             /*WebCollector automatically filters links that have been fetched before*/
@@ -66,5 +69,36 @@ public class NewsCrawler extends BreadthCrawler {
         //crawler.setResumable(true);
         /*start crawl with depth of 4*/
         crawler.start(4);
+    }
+    
+    /**
+     * 对较长文章进行分页获取
+     * 
+     * @return
+     */
+    public String getRecordForPage(String article) {
+    	//每页多少字符
+    	int pageSize = 200;
+    	int totalCount = article.length();
+    	//总共分几页
+    	int pageCount = totalCount / pageSize;
+    	if (totalCount % pageSize != 0){
+    		pageCount++;
+    	}
+    	System.out.println("总页数："+pageCount);
+    	
+    	StringBuffer sbf = new StringBuffer();
+    	for (int i = 0; i < pageCount; i++) {
+    		if (i < (pageCount - 1)) {
+    			sbf.append(article.substring(pageSize * i, pageSize * (i + 1)));
+    			System.out.println("第"+(i+1) + "页："+article.substring(pageSize * i, pageSize * (i + 1)));
+    		} else {
+    			sbf.append(article.substring(pageSize * i, article.length()));
+    			System.out.println("第"+(i+1) + "页："+article.substring(pageSize * i, article.length()));
+    		}
+    		
+    	}
+    	    	
+    	return sbf.toString();
     }
 }
